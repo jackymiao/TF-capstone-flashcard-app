bca8ac11a108dff920c75996b18fd827284bb5a1
jackymiao <55778701+jackymiao@users.noreply.github.com>
2023-08-01 09:59:55 -0500

    update readme file with a description

--- README.mk
+++ README.mk
@@ -1,1 +1,16 @@
 # FlashCard-O-Matic
+
+This is a flash card application, Flashcard-o-matic, to help students study online. Teachers will use this application to create decks of flash cards for the subjects that they teach, and students will study the decks.
+
+##Start
+Click [HERE](https://flashcard-o-matic-vr6s.onrender.com) to view live website and create your own deck to start an exciting journey!
+
+
+## Description
+-Home: Shows a list of decks with options to create, study, view, or delete a deck
+-Study: Allows the user to study the cards from a specified deck
+-Create Deck: Allows the user to create a new deck
+-Deck: Shows all of the information about a specified deck with options to edit or add cards to the deck, navigate to the study screen, or delete the deck
+-Edit Deck: Allows the user to modify information on an existing deck
+-Add Card: 	Allows the user to add a new card to an existing deck
+-Edit Card: Allows the user to modify information on an existing card
